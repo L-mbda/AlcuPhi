5c85109d93011f3b67cc1e4fc693809823dc977c
Lambda <61808223+L-mbda@users.noreply.github.com>
2025-05-01 15:42:13 -0700

    Update compile.ts

--- compile.ts
+++ compile.ts
@@ -16,7 +16,7 @@ if (isBun) {
 // Copy "./questions" → "./next/server"
 async function copyQuestionsDir() {
     const sourceDir = path.resolve('.', 'questions');
-    const destDir   = path.resolve('.', 'next', 'server');
+    const destDir   = path.resolve('.', 'next', 'server','questions');
 
     try {
         await cp(sourceDir, destDir, { recursive: true });
